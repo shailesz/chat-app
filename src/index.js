96e20eaa882fafddccb91481b28a58611dbe88ae
shailesz <sailesh.karki@sagarmatha.edu.np>
2020-09-05 11:25:01 +0545

    Fixed port issue

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
@@ -90,6 +91,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("server is up on port 3000");
 });
